2fb06f0e2d653f3581f6b511367948731047b4c6
loku <92410060+loku74@users.noreply.github.com>
2023-08-30 22:39:29 +0200

    User page added
    La div des stats sort de la balise html et la progress bar fait des warnings

---
+++ user_page/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+
+:root {
+  --pblack: #0b0c10;
+  --pdark: #1f2833;
+  --plight: #c5c6c7;
+  --pcyan: #66fcf1;
+  --pblue: #45a29e;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  height: 100%;
+  background: linear-gradient(45deg, var(--pblack), var(--pdark));
+}
+</style>
