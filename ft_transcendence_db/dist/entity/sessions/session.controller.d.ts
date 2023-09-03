72ac6215a837e2744979fc7cb45e4f1abebdeb6a
loku <92410060+loku74@users.noreply.github.com>
2023-09-03 19:58:57 +0200

    git ignore update + removed node modules and dist

--- ft_transcendence_db/dist/entity/sessions/session.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { SessionService } from './session.service';
-import { Response } from 'express';
-export declare class SessionController {
-    private readonly sessionService;
-    constructor(sessionService: SessionService);
-    pingBack(req: any, res: Response): Promise<any>;
-    getCookies(req: any): any;
-}
